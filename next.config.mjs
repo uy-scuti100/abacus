9b45dea95efd7e5454dbe5ee22d2d17dbb1ea205
Hussain Yusuf <114601628+uy-scuti100@users.noreply.github.com>
2024-09-04 01:02:40 +0100

    rewrote pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,53 +1,10 @@
-/** @type {import('next').NextConfig} */
-// import nextPWA from "next-pwa";
-import nextra from "nextra";
-
-// const withPWAConfig = nextPWA({
-// 	dest: "public",
-// });
-
-const withNextra = nextra({
-	theme: "nextra-theme-docs",
-	themeConfig: "./theme.config.tsx",
-});
-
-const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				hostname: "res.cloudinary.com",
-				protocol: "https",
-			},
-			{
-				hostname: "i.pinimg.com",
-				protocol: "https",
-			},
-			{
-				hostname: "lh3.googleusercontent.com",
-				protocol: "https",
-			},
-			{
-				hostname: "avatars.githubusercontent.com",
-				protocol: "https",
-			},
-			{
-				hostname: "avatar.vercel.sh",
-				protocol: "https",
-			},
-		],
-	},
-};
-
-// Combine both wrappers
-export default withNextra(nextConfig);
-
 // /** @type {import('next').NextConfig} */
-// import nextPWA from "next-pwa";
+// // import nextPWA from "next-pwa";
 // import nextra from "nextra";
 
-// const withPWAConfig = nextPWA({
-// 	dest: "public",
-// });
+// // const withPWAConfig = nextPWA({
+// // 	dest: "public",
+// // });
 
 // const withNextra = nextra({
 // 	theme: "nextra-theme-docs",
@@ -82,4 +39,47 @@ export default withNextra(nextConfig);
 // };
 
 // // Combine both wrappers
-// export default withPWAConfig(withNextra(nextConfig));
+// export default withNextra(nextConfig);
+
+/** @type {import('next').NextConfig} */
+import nextPWA from "next-pwa";
+import nextra from "nextra";
+
+const withPWAConfig = nextPWA({
+	dest: "public",
+});
+
+const withNextra = nextra({
+	theme: "nextra-theme-docs",
+	themeConfig: "./theme.config.tsx",
+});
+
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				hostname: "res.cloudinary.com",
+				protocol: "https",
+			},
+			{
+				hostname: "i.pinimg.com",
+				protocol: "https",
+			},
+			{
+				hostname: "lh3.googleusercontent.com",
+				protocol: "https",
+			},
+			{
+				hostname: "avatars.githubusercontent.com",
+				protocol: "https",
+			},
+			{
+				hostname: "avatar.vercel.sh",
+				protocol: "https",
+			},
+		],
+	},
+};
+
+// Combine both wrappers
+export default withPWAConfig(withNextra(nextConfig));
